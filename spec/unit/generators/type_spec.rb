0b58dff1bcaceeab4ee3b3b6918a32cc42d70698
Corey Osman <corey@logicminds.biz>
2015-11-13 15:11:36 -0800

    update test and add fixture data

--- spec/unit/generators/type_spec.rb
+++ spec/unit/generators/type_spec.rb
@@ -54,4 +54,40 @@ describe 'type' do
       expect(models.instance_methods).to eq([])
     end
   end
+
+  describe 'opatch' do
+    let(:file) do
+      File.join(fixtures_type_path, 'db_opatch.rb')
+    end
+
+    let(:models) do
+      Retrospec::Puppet::Type.load_type(file)
+    end
+
+    it 'can eval code' do
+      models = Retrospec::Puppet::Type.load_type(file)
+      expect(models.name).to eq(:db_opatch)
+    end
+
+    it 'has correct amount of properties' do
+      expect(models.properties).to eq([:ensure])
+    end
+
+    it 'has correct amount of parameters' do
+      expect(models.parameters).to eq([:name,
+                                        :patch_id,
+                                        :os_user,
+                                        :oracle_product_home_dir,
+                                        :orainst_dir,
+                                        :extracted_patch_dir,
+                                        :ocmrf_file,
+                                        :opatch_auto,
+                                        :bundle_sub_patch_id])
+    end
+
+    it 'has the correct number of instance methods' do
+      expect(models.instance_methods).to eq([])
+    end
+
+  end
 end
